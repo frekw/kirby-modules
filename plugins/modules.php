7bd41b31765a2c5ab080fd50da38f144029b7a25
frekw <fredrik@soundtrackyourbrand.com>
2016-03-30 10:19:48 +0200

    Only combine assets if explicitly enabled.

--- plugins/modules.php
+++ plugins/modules.php
@@ -3,7 +3,9 @@
 kirby()->roots->modules = kirby()->roots->site() . DS . 'modules';
 
 // TODO: if c::get yadda yadda
-require_once __DIR__ . DS . 'modules' . DS . 'router.php';
+if(c::get('modules.assets') === true){
+  require_once __DIR__ . DS . 'modules' . DS . 'router.php';
+}
 require_once __DIR__ . DS . 'modules' . DS . 'renderer.php';
 
 field::$methods['modules'] = function($field) {
