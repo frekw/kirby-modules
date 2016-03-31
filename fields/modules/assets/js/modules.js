4ccd2e96d126e07fc605255fd35074f5a9241b1b
frekw <fredrik@soundtrackyourbrand.com>
2016-03-31 10:57:19 +0200

    Modules are now accordions. Closes #2.

--- fields/modules/assets/js/modules.js
+++ fields/modules/assets/js/modules.js
@@ -45,6 +45,50 @@
 })(jQuery);
 
 (function($) {
+  var defaults = {
+    toggle: '.accordion-toggle',
+    content: '.accordion-content'
+  };
+
+  var Accordion = function(el, options){
+    this.$el = $(el);
+    this.options = options;
+
+    this.toggle = this.toggle.bind(this);
+
+    this.$el.on('click', options.toggle, function(e){
+      e.preventDefault();
+      e.stopPropagation();
+
+      this.toggle($(e.target));
+    }.bind(this));
+
+    this.$el.find(options.toggle).each(function(i, el){
+      this.toggle($(el));
+    }.bind(this));
+  };
+
+  Accordion.prototype.toggle = function($target){
+    if($target.is('.accordion--closed')){
+      $target.removeClass('accordion--closed').addClass('accordion--open');
+      $target.next(this.options.content).show();
+    } else {
+      $target.removeClass('accordion--open').addClass('accordion--closed');
+      $target.next(this.options.content).hide();
+    }
+  };
+
+  $.fn.accordion = function(options){
+    options = options || {};
+    options = $.extend({}, defaults, options);
+
+    return this.each(function(){
+      new Accordion(this, options);
+    });
+  };
+})(jQuery);
+
+(function($) {
 
   var Modules = function(el) {
     var element  = $(el);
@@ -52,6 +96,9 @@
     var sortable = element.data('sortable');
 
     element.find('.tabs').tabs();
+    element.accordion({
+      toggle: '> .modules-entries > .modules-entry > .accordion-toggle'
+    });
 
     if(sortable === false) return false;
 
--- fields/modules/templates/items.php
+++ fields/modules/templates/items.php
@@ -4,36 +4,38 @@ $form = require implode(DS, array(__DIR__, 'form.php'));
 foreach($field->entries() as $i => $entry): ?>
 
 <div class="modules-entry" id="modules-entry-<?php echo $entry->id() ?>">
-  <h4 class="modules-type"><?php echo ucfirst($entry->type()); ?> Module</h4>
+  <h4 class="modules-type accordion-toggle"><?php echo ucfirst($entry->type()); ?> Module</h4>
+    <div class="accordion-content">
 
-  <?php if(isset($entry->options)): ?>
-  <ul class="tabs">
-    <li><a href="#modules-entry-fields-<?php echo $entry->id() ?>">Content</a></li>
-    <li><a href="#modules-entry-options-<?php echo $entry->id() ?>">Settings</a></li>
-  </ul>
-  <?php endif; ?>
+    <?php if(isset($entry->options)): ?>
+    <ul class="tabs">
+      <li><a href="#modules-entry-fields-<?php echo $entry->id() ?>">Content</a></li>
+      <li><a href="#modules-entry-options-<?php echo $entry->id() ?>">Settings</a></li>
+    </ul>
+    <?php endif; ?>
 
-  <?php if(!$field->readonly()): ?>
-    <a data-modal
-       class="btn btn-icon btn-rounded btn-negative modules-entry-delete"
-       href="<?php __($field->url('delete') . '/' . $entry->id()) ?>">
-    <?php i('trash-o', 'left') . _l('fields.structure.delete') ?>
-  </a>
-  <?php endif; ?>
-
-  <div class="modules-entry-content">
     <?php if(!$field->readonly()): ?>
-      <div class="modules-entry-fields" id="modules-entry-fields-<?php echo $entry->id() ?>">
-        <?php $form('fields', $entry, $field); ?>
-      </div>
+      <a data-modal
+         class="btn btn-icon btn-rounded btn-negative modules-entry-delete"
+         href="<?php __($field->url('delete') . '/' . $entry->id()) ?>">
+      <?php i('trash-o', 'left') . _l('fields.structure.delete') ?>
+    </a>
+    <?php endif; ?>
 
-      <?php if(isset($entry->options)): ?>
-        <div class="modules-entry-options" id="modules-entry-options-<?php echo $entry->id() ?>">
-          <?php $form('options', $entry, $field); ?>
+    <div class="modules-entry-content">
+      <?php if(!$field->readonly()): ?>
+        <div class="modules-entry-fields" id="modules-entry-fields-<?php echo $entry->id() ?>">
+          <?php $form('fields', $entry, $field); ?>
         </div>
-      <?php endif; ?>
-    <?php require implode(DS, array(__DIR__, 'form.php')); ?>
-    <?php endif ?>
+
+        <?php if(isset($entry->options)): ?>
+          <div class="modules-entry-options" id="modules-entry-options-<?php echo $entry->id() ?>">
+            <?php $form('options', $entry, $field); ?>
+          </div>
+        <?php endif; ?>
+      <?php require implode(DS, array(__DIR__, 'form.php')); ?>
+     <?php endif ?>
+  </div>
   </div>
 </div>
 <?php endforeach ?>
